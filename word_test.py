92f37b0c5f4513da4200cbf24fd8236cb8361741
dhanush183 <dhanush.janapareddy2002@gmail.com>
2024-04-27 22:30:53 +0200

    integration testing

--- word_test.py
+++ word_test.py
@@ -5,10 +5,10 @@ from word_processor import WordProcessor
 class Integration(unittest.TestCase):
     def test(self):
         text = '12madam21'
-        self.assertEqual(WordProcessor.count_numbers(self, text), 4)
-        self.assertEqual(WordProcessor.count_alphabets(self, text), 5)
-        self.assertEqual(WordProcessor.is_palindrome(self, text), True)
-        self.assertEqual(WordProcessor.replace_string(self, text, 'a', 's'), '12msdsm21')
+        self.assertEqual(WordProcessor.count_numbers(self, text), 4) #test for numbers count
+        self.assertEqual(WordProcessor.count_alphabets(self, text), 5) #test for alphabets count
+        self.assertEqual(WordProcessor.is_palindrome(self, text), True) #test for checking palindrome or not
+        self.assertEqual(WordProcessor.replace_string(self, text, 'a', 's'), '12msdsm21') #test for replace the string
 
 
 if __name__ == "__main__":
