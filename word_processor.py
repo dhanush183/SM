0cbf19cc5b3e684370b88024683a9e204df81d45
dhanush183 <dhanush.janapareddy2002@gmail.com>
2024-04-27 19:07:44 +0200

    now pushing the code into the repository with invalid output with giving  incorrect expected output

--- word_processor.py
+++ word_processor.py
@@ -1,18 +1,15 @@
 
 class WordProcessor:
-"""
 # method to generate count of alphabets in a string
-   def count_alphabets(self, text):
-        return sum(1 for char in text if char.isalpha())
-"""
+ #  def count_alphabets(self, text):
+ #       return sum(1 for char in text if char.isalpha())
+
 # method to generate count of numbers in a string
     def count_numbers(self, text):
         return sum(1 for char in text if char.isdigit())
-"""
-    def is_palindrome(self, text):
-        stripped_text = ''.join(char.lower() for char in text if char.isalnum())
-        return stripped_text == stripped_text[::-1]
 
-    def replace_string(self, text, old, new):
-        return text.replace(old, new)
-"""
+  #  def is_palindrome(self, text):
+   #     return stripped_text == stripped_text[::-1]
+
+  #  def replace_string(self, text, old, new):
+   #     return text.replace(old, new)
--- word_test.py
+++ word_test.py
@@ -10,8 +10,8 @@ def test_count_alphabets():
     assert result == exp_result, f"Expected: {exp_result}, but got: {result}"
 """
 def test_count_numbers():
-    text = "Hello World 123" # This line is the input text for count numbers testing
-    exp_result = 3 # this line is for expected output
+    text = "Hello World 123"# This line is the input text for count numbers testing
+    exp_result = 4# this line is for expected output
     result = word_processor.count_numbers(text)
     assert result == exp_result, f"Expected: {exp_result}, but got: {result}"
 
