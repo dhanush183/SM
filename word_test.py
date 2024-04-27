658173d73b4409e32ecd828c9a5672169c302452
dhanush183 <dhanush.janapareddy2002@gmail.com>
2024-04-27 15:47:21 +0200

    trail test 2

--- word_test.py
+++ word_test.py
@@ -5,7 +5,7 @@ word_processor = WordProcessor()
 
 def test_count_alphabets():
     text = "Hello World 123"
-    exp_result = 10
+    exp_result = 101
     result = word_processor.count_alphabets(text)
     assert result == exp_result, f"Expected: {exp_result}, but got: {result}"
 
